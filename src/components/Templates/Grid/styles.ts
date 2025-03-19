2996388bbb8518b40beff41988d27ab2ad2eabfc
Larissa Giaccon <larissa.giaccon@leanwork.com.br>
2025-03-18 22:03:59 -0300

    Fix: Ajuste estilização da grid

--- src/components/Templates/Grid/styles.ts
+++ src/components/Templates/Grid/styles.ts
@@ -2,12 +2,14 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
     display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(20rem, 1fr));
+    grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+    justify-content: center;
     gap: 1rem;
     padding: 2rem;
     width: 100%;
 
     @media (max-width: 768px) {
         grid-template-columns: var(--default_showcase_mobile);
+        grid-row-gap: 2rem;
     }
 `
