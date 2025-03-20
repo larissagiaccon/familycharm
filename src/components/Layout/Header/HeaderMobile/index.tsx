4ce5d7df3e498f372467e7e6311dbf8d7e63eb9c
Larissa Giaccon <larissa.giaccon@leanwork.com.br>
2025-03-20 16:06:31 -0300

    Feat: Adicionado mini carrinho fake no mobile para teste

--- src/components/Layout/Header/HeaderMobile/index.tsx
+++ src/components/Layout/Header/HeaderMobile/index.tsx
@@ -5,6 +5,8 @@ import debounce from 'lodash.debounce'
 import { SlBasket } from 'react-icons/sl'
 import { FiMenu, FiSearch, FiX } from 'react-icons/fi'
 
+import MiniCart from 'components/Layout/MiniCart'
+
 import { cart, config } from 'mocks' // TODO REMOVER MOCK
 
 import * as S from './styles'
@@ -12,6 +14,11 @@ import * as S from './styles'
 export default function HeaderMobile() {
     const [showSearch, setShowSearch] = useState(false)
     const [closingSearch, setClosingSearch] = useState(false)
+    const [showMiniCart, setShowMiniCart] = useState(false) // TODO REMOVER MOCK
+
+    function dropMiniCart() {
+        setShowMiniCart(false)
+    } // TODO REMOVER MOCK
 
     const handleScroll = debounce(() => {
         if (typeof window !== 'undefined') {
@@ -31,58 +38,65 @@ export default function HeaderMobile() {
     }, [])
 
     return (
-        <S.Container className="header-mobile" id="header-mobile">
-            <div id="height-swap-mobile" />
+        <>
+            {/* TODO REMOVER QUANDO ADICIONAR HOOK CARRINHO */}
+            {showMiniCart && <MiniCart onClose={dropMiniCart} />}{' '}
+            <S.Container className="header-mobile" id="header-mobile">
+                <div id="height-swap-mobile" />
 
-            <div className="content-mobile">
-                <FiMenu />
+                <div className="content-mobile">
+                    <FiMenu />
 
-                <Link href="/" passHref>
-                    <a href="">
-                        <img
-                            src={config.aparencia.logo}
-                            alt={config.loja.nome}
-                            className="logo-mobile"
-                        />
-                    </a>
-                </Link>
+                    <Link href="/" passHref>
+                        <a href="">
+                            <img
+                                src={config.aparencia.logo}
+                                alt={config.loja.nome}
+                                className="logo-mobile"
+                            />
+                        </a>
+                    </Link>
 
-                <div className="header-mobile-group">
-                    {/* <FiSearch
-                        onClick={() => {
-                            setShowSearch(true)
-                        }}
-                    /> */}
+                    <div className="header-mobile-group">
+                        {/* <FiSearch
+                                    onClick={() => {
+                                        setShowSearch(true)
+                                    }}
+                                /> */}
 
-                    <div className="cart-itens">
-                        <SlBasket />
-                        <p>{cart.totalItens}</p>
+                        <div
+                            className="cart-itens"
+                            onClick={() => setShowMiniCart(true)}
+                        >
+                            <SlBasket />
+                            <p>{cart.totalItens}</p>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            {showSearch && (
-                <div className={`search${closingSearch ? ' closing' : ''}`}>
-                    <div className="input-search">
-                        <input type="text" placeholder="Pesquisar..." />
-                        <button type="submit">
-                            <FiSearch />
-                        </button>
+                {showSearch && (
+                    <div className={`search${closingSearch ? ' closing' : ''}`}>
+                        <div className="input-search">
+                            <input type="text" placeholder="Pesquisar..." />
+                            <button type="submit">
+                                <FiSearch />
+                            </button>
+                        </div>
+                        <div
+                            className="close-search"
+                            onClick={() => {
+                                setClosingSearch(true)
+                                setTimeout(() => {
+                                    setShowSearch(false)
+                                    setClosingSearch(false)
+                                }, 100)
+                            }}
+                        >
+                            <FiX />
+                        </div>
                     </div>
-                    <div
-                        className="close-search"
-                        onClick={() => {
-                            setClosingSearch(true)
-                            setTimeout(() => {
-                                setShowSearch(false)
-                                setClosingSearch(false)
-                            }, 100)
-                        }}
-                    >
-                        <FiX />
-                    </div>
-                </div>
-            )}
-        </S.Container>
+                )}
+            </S.Container>
+        </>
     )
 }
