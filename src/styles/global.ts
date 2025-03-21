441b6b7eaa17078df549995a698b21c44bbf8a56
Larissa Giaccon <larissa.giaccon@leanwork.com.br>
2025-03-21 03:19:49 -0300

    Fix: Titulo da seção global

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -149,4 +149,55 @@ export default createGlobalStyle`
     .selected:not(.unavailable) {
         border: 1px solid #000;
     }
+
+    .section-title {
+        display: flex;
+        align-items: center;
+        font-weight: 400;
+        white-space: nowrap;
+        justify-self: center;
+        position: relative;
+        gap: 4rem;
+        padding: 0 1em;
+
+        &::before,
+        &::after {
+            content: '';
+            display: block;
+            width: 20rem;
+            height: 1px;
+            background-color: var(--primary_color);
+            opacity: 0.8;
+        }
+    }
+
+
+    @media (max-width: 768px) {
+        .section-title {
+            gap: 1rem;
+            padding: 0 0.5em;
+
+            &::before,
+            &::after {
+                width: 10rem;
+            }
+        }
+    }
+
+    @media (max-width: 410px) {
+        .section-title {
+            gap: 0;
+            padding: 0;
+            white-space: normal;
+            flex-direction: column;
+
+            &::after {
+                margin-top: 0.3rem;
+            }
+
+            &::before {
+                width: 0;
+            }
+        }
+    }
 `
